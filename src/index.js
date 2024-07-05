b57eac140cef983b472764b2c3cdc4c7f4cac8ea
Dhruv Mehta <flashxx001@gmail.com>
2024-07-05 19:57:45 +0530

    React Fragments.

--- src/index.js
+++ src/index.js
@@ -69,18 +69,27 @@ function Header() {
   );
 }
 function Menu() {
+  // const pizzas = [];
   const pizzas = pizzaData;
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-            // <Pizza name={pizza.name} photoName={pizza.photoName} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            fromour stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+              // <Pizza name={pizza.name} photoName={pizza.photoName} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're Still working on Our Menu, Please come back later.</p>
       )}
